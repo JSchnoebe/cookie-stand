a983c8d2bd8a0c3845fa249494d265231df5b970
tektechnologies <info@tektechnologies.com>
2021-08-09 19:55:24 -0500

    add totals  TH to TR then to the TABLE

--- js/app.js
+++ js/app.js
@@ -89,27 +89,28 @@ function makeFooterRow(){
   const tableRow = document.createElement('tr');
   let tableHeaderElement = document.createElement('th');
   tableHeaderElement.textContent = 'Totals';
-  tableFooter.appendChild(tableHeaderElement);
-
-
-  let totalOfTotals = 0;
-
-  for(let i = 0; i < hours.length; i++){
-
-    let hourlyTotals = 0;
-
-    for(let j = 0; j < StoreCreator.allStoresArray.length; j++){
-      hourlyTotals += StoreCreator.allStoresArray[j].cookiesEachHour[i];
-      totalOfTotals = totalOfTotals + StoreCreator.allStoresArray[j].cookiesEachHour[i];
-    }
-    tableHeaderElement = document.createElement('th');
-    tableHeaderElement.textContent = hourlyTotals;
-    tableRow.appendChild(tableHeaderElement);
-  }
-  tableHeaderElement = document.createElement('th');
-  tableHeaderElement.textContent = totalOfTotals;
   tableRow.appendChild(tableHeaderElement);
   tableFooter.appendChild(tableRow);
+
+
+  // let totalOfTotals = 0;
+
+  // for(let i = 0; i < hours.length; i++){
+
+  //   let hourlyTotals = 0;
+
+  //   for(let j = 0; j < StoreCreator.allStoresArray.length; j++){
+  //     hourlyTotals += StoreCreator.allStoresArray[j].cookiesEachHour[i];
+  //     totalOfTotals = totalOfTotals + StoreCreator.allStoresArray[j].cookiesEachHour[i];
+  //   }
+  //   tableHeaderElement = document.createElement('th');
+  //   tableHeaderElement.textContent = hourlyTotals;
+  //   tableRow.appendChild(tableHeaderElement);
+  // }
+  // tableHeaderElement = document.createElement('th');
+  // tableHeaderElement.textContent = totalOfTotals;
+  // tableRow.appendChild(tableHeaderElement);
+  // tableFooter.appendChild(tableRow);
 
 }
 
